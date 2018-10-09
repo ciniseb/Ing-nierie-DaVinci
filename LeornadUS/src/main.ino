1b0c28679d5d1a71b9c1ab85b6797790b13f7a0e
PhilBL42 <32170226+PhilBL42@users.noreply.github.com>
2018-10-09 11:59:08 -0400

    Implémentation de l'accélération
    -Encore du fine tuning à apporter au angles et aux distance dans le parcour
    
    -I de PI à implémenter

--- LeornadUS/src/main.ino
+++ LeornadUS/src/main.ino
@@ -19,10 +19,12 @@ Variables globales et defines
 **************************************************************************** */
 // -> defines...
 // L'ensemble des fonctions y ont acces
-#define GAUCHE 0
-#define DROITE 1
-#define speed0 0
-#define speed1 0.5
+#define GAUCHE  0
+#define DROITE  1
+#define speed0  0
+#define speed1  0.5
+#define ACCEL   1
+#define DECCEL  0
 
 
 /* ****************************************************************************
@@ -55,9 +57,9 @@ float dPICalc(float distancegauche1, float distancedroite1)
   // PI = 40 -> 40 tick de plus a faire.
   // Calcul PI en pulse.
   dPIout = (dPout+dIout)/100;
-  Serial.print("PIOUT: ");
-  Serial.println(dPIout);
-  Serial.println(" ");
+  //Serial.print("PIOUT: ");
+  //Serial.println(dPIout);
+  //Serial.println(" ");
   return (dPIout);
 }
 
@@ -98,6 +100,7 @@ float angle_degree_a_pulse(float angle)
 int tourner(int direction, float angle)
 {
   float angle_pulse;
+  bool bPremFois = 1;
   //détermine le nombre de pulse pour arriver à l'angle demandé
   angle_pulse = angle_degree_a_pulse(angle);
 
@@ -106,8 +109,10 @@ int tourner(int direction, float angle)
   {
     while(ENCODER_Read(DROITE)<=angle_pulse)
     {
-        MOTOR_SetSpeed(GAUCHE,speed0);
-        MOTOR_SetSpeed(DROITE,speed1);
+      //vSendSpeed(speed0,speed1,ACCEL, bPremFois);
+      //bPremFois = 0;
+      MOTOR_SetSpeed(GAUCHE,speed0);
+      MOTOR_SetSpeed(DROITE,speed1);
     }
   
   }
@@ -115,13 +120,19 @@ int tourner(int direction, float angle)
   {
     while(ENCODER_Read(GAUCHE)<=angle_pulse)
     {
-        MOTOR_SetSpeed(GAUCHE,speed1);
-        MOTOR_SetSpeed(DROITE,speed0);
+      //vSendSpeed(speed1,speed0,ACCEL,bPremFois);
+      //bPremFois = 0;
+      MOTOR_SetSpeed(GAUCHE,speed1);
+      MOTOR_SetSpeed(DROITE,speed0);
     }
   }
-    MOTOR_SetSpeed(GAUCHE,speed0);
-    MOTOR_SetSpeed(DROITE,speed0);
-    transition();
+
+  //vSendSpeed(speed0,speed0,DECCEL,bPremFois);
+  //bPremFois = 0;
+  MOTOR_SetSpeed(GAUCHE,speed0);
+  MOTOR_SetSpeed(DROITE,speed0);
+  transition();
+  
   return 0;
 }
 
@@ -134,6 +145,7 @@ float distance_mm_pulse(float distance_mm)
   float distance_pulse = (distance_mm/circonference_roue_mm)*circonference_roue_pulse;
   return distance_pulse;
 }
+
 void avancer(float distance_mm)
 {
   float distance_pulse,distgauche1,distdroite1;
@@ -142,29 +154,54 @@ void avancer(float distance_mm)
   float accel=0;
   int counter=0;
   distance_pulse = distance_mm_pulse(distance_mm);
+  bool bPremFois = 1;
   while(ENCODER_Read(GAUCHE)<=distance_pulse)
   {
-    if(counter=0)
+    Serial.println(counter);
+    if(counter == 0)
     {
+      counter = 1;
+      for(float f = 0; f <= 0.5 ; f = f + 0.1)
+      {
+        Serial.println("ok");
+        MOTOR_SetSpeed(GAUCHE,f);
+        MOTOR_SetSpeed(DROITE,f);
+        delay(25);
+      }
+    }
+    /*
+    if(counter==0)
+    {
+      Serial.println("ok");
       for (int i = 0; i < 3; i++) 
-      {     
+      {
         accel = accel + 0.1;
         MOTOR_SetSpeed(GAUCHE,accel);
         MOTOR_SetSpeed(DROITE,accel);
+        Serial.println(" ");
+        Serial.println("acceleration");
         delay(25);
       }
       counter=1;
     }
+    */
     distgauche1 = ENCODER_Read(GAUCHE);
     distdroite1 = ENCODER_Read(DROITE);
     k=dPICalc(distgauche1,distdroite1);
     speed2 = speed1+k;
 
-        MOTOR_SetSpeed(GAUCHE,speed1);
-        MOTOR_SetSpeed(DROITE,speed2);
-           delay(100);
+    // À TESTER PEUX NE PAS FONCTIONNER CORRECTEMENT!
+    //vSendSpeed(speed1,speed2,ACCEL, bPremFois);
+    //bPremFois = 0;
+    MOTOR_SetSpeed(GAUCHE,speed1);
+    MOTOR_SetSpeed(DROITE,speed2);
+    delay(100);
   }
 
+  bPremFois = 1;
+
+  //vSendSpeed(speed0,speed0,DECCEL, bPremFois);
+  //bPremFois = 0;
   MOTOR_SetSpeed(GAUCHE,speed0);
   MOTOR_SetSpeed(DROITE,speed0);
   transition();
@@ -175,18 +212,37 @@ void reculer(float distance_mm)
   float distance_pulse,distgauche1,distdroite1;
   float k;
   float speed2= -speed1;
+  bool counter = 0;
+  bool bPremFois = 1;
   distance_pulse = distance_mm_pulse(distance_mm);
   while(ENCODER_Read(GAUCHE)>=-distance_pulse)
   {
+    if(counter == 0)
+    {
+      counter = 1;
+      for(float f = 0; f >= -0.5 ; f = f - 0.1)
+      {
+        Serial.println("ok");
+        MOTOR_SetSpeed(GAUCHE,f);
+        MOTOR_SetSpeed(DROITE,f);
+        delay(25);
+      }
+    }
+
     distgauche1 = ENCODER_Read(GAUCHE);
     distdroite1 = ENCODER_Read(DROITE);
     k=dPICalc(distgauche1,distdroite1);
     speed2 = -speed1+k;
-        MOTOR_SetSpeed(GAUCHE,-speed1);
-        MOTOR_SetSpeed(DROITE,speed2);
-           delay(100);
+    
+    //vSendSpeed(-speed1,speed2,ACCEL, bPremFois);
+    //bPremFois = 0;
+    MOTOR_SetSpeed(GAUCHE,-speed1);
+    MOTOR_SetSpeed(DROITE,speed2);
+    delay(100);
   }
 
+
+  //vSendSpeed(speed0,speed0,DECCEL);
   MOTOR_SetSpeed(GAUCHE,speed0);
   MOTOR_SetSpeed(DROITE,speed0);
   transition();
@@ -195,6 +251,7 @@ void reculer(float distance_mm)
 int tourner_reculer(int direction, float angle)
 {
   float angle_pulse;
+  bool bPremFois = 1;
   //détermine le nombre de pulse pour arriver à l'angle demandé
   angle_pulse = angle_degree_a_pulse(angle);
 
@@ -203,8 +260,10 @@ int tourner_reculer(int direction, float angle)
   {
     while(ENCODER_Read(DROITE)>=-angle_pulse)
     {
-        MOTOR_SetSpeed(GAUCHE,speed0);
-        MOTOR_SetSpeed(DROITE,-speed1);
+      //vSendSpeed(speed0,-speed1,ACCEL, bPremFois);
+      //bPremFois = 0;
+      MOTOR_SetSpeed(GAUCHE,speed0);
+      MOTOR_SetSpeed(DROITE,-speed1);
     }
   
   }
@@ -212,14 +271,69 @@ int tourner_reculer(int direction, float angle)
   {
     while(ENCODER_Read(GAUCHE)>=-angle_pulse)
     {
-        MOTOR_SetSpeed(GAUCHE,-speed1);
-        MOTOR_SetSpeed(DROITE,speed0);
+      //vSendSpeed(-speed1,speed0,ACCEL, bPremFois);
+      //bPremFois = 0;
+      MOTOR_SetSpeed(GAUCHE,-speed1);
+      MOTOR_SetSpeed(DROITE,speed0);
     }
   }
-    MOTOR_SetSpeed(GAUCHE,speed0);
-    MOTOR_SetSpeed(DROITE,speed0);
-    transition();
+  MOTOR_SetSpeed(GAUCHE,speed0);
+  MOTOR_SetSpeed(DROITE,speed0);
+  transition();
   return 0;
+}
+
+void vSendSpeed(float fVitesseMotGauche, float fVitesseMotDroite, bool bAccelStop, bool bPremFois)
+{
+  float static fAcienneVitesseMotGauche = 0;
+  float static fAcienneVitesseMotDroite = 0;
+  
+  Serial.print("Ancienne vitesse: ");
+  Serial.println(fAcienneVitesseMotGauche);
+  Serial.print("Ancienne vitesse: ");
+  Serial.println(fVitesseMotGauche);
+  // Vitesse à changé ?
+  if(bPremFois)
+  {
+    fAcienneVitesseMotGauche = fVitesseMotGauche;
+    fAcienneVitesseMotDroite = fVitesseMotDroite;
+
+    //Serial.print("Ancienne nouvelle vitesse: ");
+    //Serial.println(fAcienneVitesseMotGauche);
+
+    if(bAccelStop == ACCEL)
+    {
+      for(float f = 0; f < 0.5 ; f = f + 0.1)
+      {
+        if(f <= fVitesseMotGauche)
+        {
+          MOTOR_SetSpeed(GAUCHE,f);
+        }
+        if(f <= fVitesseMotDroite)
+        {
+          MOTOR_SetSpeed(DROITE,f);
+        }
+        delay(25);
+      }
+    }
+    else
+    {
+      Serial.println(" ");
+      Serial.println("décélère");
+      for(float f = 0.5; f > 0 ; f = f - 0.1)
+      {
+        if(fVitesseMotGauche == 0)
+        {
+          MOTOR_SetSpeed(GAUCHE,f);
+        }
+        if(fVitesseMotDroite == 0)
+        {
+          MOTOR_SetSpeed(DROITE,f);
+        }
+        delay(25);
+      }
+    }
+  }
 }
 
 /* ****************************************************************************
@@ -231,6 +345,8 @@ Fonctions d'initialisation (setup)
 
 void setup(){
   BoardInit();
+
+  
 }
 
 
@@ -243,21 +359,48 @@ void loop()
  {
   // SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
   delay(10);// Delais pour décharger le CPU
+
+// test d'accélération
+  if(ROBUS_IsBumper(1))
+  {
+    vSendSpeed(speed1,speed1,ACCEL,1);
+    delay(1000);
+    vSendSpeed(speed1,speed0,DECCEL,1);
+
+    while(1);
+    
+  }
+
+// test d'accélération
+  if(ROBUS_IsBumper(2))
+  {
+    for(float i; i < 0.5 ; i = i + 0.1)
+    {
+      MOTOR_SetSpeed(GAUCHE,i);
+      MOTOR_SetSpeed(DROITE,i);
+      delay(100);
+    }
+    MOTOR_SetSpeed(GAUCHE,speed1);
+    MOTOR_SetSpeed(DROITE,speed1);
+  }
+
+
   if(ROBUS_IsBumper(3))
- {
+  {
   //a-b
   avancer(2020);
+
   tourner(GAUCHE,90);
   //b-c
   avancer(300);
 
   tourner(DROITE,90);
   //c-d
-  avancer(280);
+  avancer(270);
 
-  tourner(DROITE,90);
+  tourner(DROITE,92);
   //d-e
-  avancer(300);
+  avancer(310);
 
   tourner(GAUCHE,90);
   //e-f
@@ -269,7 +412,7 @@ void loop()
   
   tourner(GAUCHE,85);
   //g-h
-  avancer(600);
+  avancer(530);
 
   tourner(DROITE,50);
   //h-i
@@ -304,15 +447,15 @@ void loop()
   
   reculer(300);
 
-  tourner_reculer(DROITE,90);
+  tourner_reculer(DROITE,92);
 
-  reculer(270);
+  reculer(250);
 
   tourner_reculer(DROITE,90);
 
   reculer(310);
 
-  tourner_reculer(GAUCHE,90);
+  tourner_reculer(GAUCHE,89);
 
   reculer(2020);
  }
