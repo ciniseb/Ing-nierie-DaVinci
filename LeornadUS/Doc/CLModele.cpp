018ea95b48d9a56cf6d0ea3e02df1979fdae5e03
PhilBL42 <32170226+PhilBL42@users.noreply.github.com>
2018-11-08 10:33:55 -0500

    Normalisation et ajout de fichier type

---
+++ LeornadUS/Doc/CLModele.cpp
@@ -0,0 +1,20 @@
+//***********************************************************************
+// Fichier:         CLModele.cpp
+// Auteur:          Auteur
+//
+// Date:            16-02-2017
+// Revision :       A
+// Compilateur:     Arduino IDE
+// Description:     Modèle de classes pour en créer plus rapidement.
+//
+//
+// Modification:    xx-xx-xxxx
+//
+//***********************************************************************
+//#include "CLModele.h"
+
+/*void main(void)
+{
+  class CLModele clModele; // Classe devien objet.
+  
+}*/
---
+++ LeornadUS/Doc/CLModele.h
@@ -0,0 +1,56 @@
+//***********************************************************************
+// Fichier:         CLModele.h
+// Auteur:          Auteur
+//
+// Date:            16-02-2017
+// Revision :       A
+// Compilateur:     Arduino IDE
+// Description:     Modèle de classes pour en créer plus rapidement.
+//
+//
+// Modification:   xx-xx-xxxx
+//
+//***********************************************************************
+/*
+ifndef CLMODELH
+  define CLMODELH
+  class CLModele
+  {
+    public:        // Membre(s) public(s):
+    void CLModele(int iA, int iB);
+    protected:
+    private:       // Membre(s) protégé(s):
+    int iHaut, iBas;
+  };
+  
+  void CLModele :: CLModele(void)
+  {
+    // Initialisation des variables:
+    iHaut = iA;
+    iBas  = iB;
+  }
+
+endif
+
+
+
+
+
+
+
+//***********************************************************************
+// Nom de la fct:   x  Fait par: Auteur
+// Date:               xx-xx-xxxx
+// Revision :          A
+// Modification :   
+// Prototype:          x x(x);
+//
+// Description:        x
+//   
+// Parametre d'entre:  - x:x
+//
+// Parametre de sortie:- x:x
+//
+// Exemple d'appel:    x(x);
+//***********************************************************************
+*/
