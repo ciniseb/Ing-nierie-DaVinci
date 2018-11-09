a34509faf385d59660f8a5dd56e5dc55bc562b53
Sébastien St-Denis <sebastienst-denis@hotmail.com>
2018-11-08 19:21:34 -0500

    LéonardUS UI
    Ajout du code de base pour l'interface des robots!

---
+++ LeonardUSUI/app/src/main/java/com/ingenieriedavinci/leonardusui/Main.java
@@ -0,0 +1,33 @@
+package com.ingenieriedavinci.leonardusui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main extends AppCompatActivity
+{
+    //Variables
+    Button debut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+
+        //Attribution
+        debut = (Button) findViewById(R.id.debut);
+
+        //Comportements
+        debut.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(Main.this, Formes.class));//Ouvrir l'activité des formes
+            }
+        });
+    }
+}
---
+++ LeonardUSUI/app/src/main/java/com/ingenieriedavinci/leonardusui/Telecommande.java
@@ -0,0 +1,14 @@
+package com.ingenieriedavinci.leonardusui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Telecommande extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_telecommande);
+    }
+}
