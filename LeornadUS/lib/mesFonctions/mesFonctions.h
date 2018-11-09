537ae8c40e0bae77b41022dbfefabf9c45a7e5a4
Sébastien St-Denis <sebastienst-denis@hotmail.com>
2018-11-08 22:42:03 -0500

    Defines des robots

--- LeornadUS/lib/mesFonctions/mesFonctions.h
+++ LeornadUS/lib/mesFonctions/mesFonctions.h
@@ -1,32 +1,55 @@
-#ifndef MESFONCTIONS_H
-#define MESFONCTIONS_H
+// ----- G É N É R A L ------
+#ifndef GENERAL
+    #define GENERAL
 
-#include <LibRobus.h>
-#include <Arduino.h>
+    #include <LibRobus.h>
+    #include <Arduino.h>
 
-//Defines
-#define GAUCHE 0
-#define DROITE 1
-#define speed0 0
-#define speed1 0.4
-#define speed2 0.25
-#define speed3 0.3
-#define speed4 0.35
+    //Defines
+    #define GAUCHE 0
+    #define DROITE 1
+    #define speed0 0
+    #define speed1 0.4
+    #define speed2 0.25
+    #define speed3 0.3
+    #define speed4 0.35
 
-//Variables
-float vitesse;
+    //Variables
+    float vitesse;
+    
+    //Fonctions
+    float PICalcul(float distanceGauche, float distanceDroite);
+    void acceleration(float v, float vMax, float distance);
+    void MOTORS_reset();
+    float angle_degree_a_pulse(float angle);
+    void tourner(int direction, float angle);
+    void tournerCentre(int direction, float angle);
+    void danse(/*float angle*/);
+    float distance_mm_pulse(float distance_mm);
+    void avancer(float distance_mm);
+    void reculer(float distance_mm);
+    void tourner_reculer(int direction, float angle);
 
-//Fonctions
-float PICalcul(float distanceGauche, float distanceDroite);
-void acceleration(float v, float vMax, float distance);
-void MOTORS_reset();
-float angle_degree_a_pulse(float angle);
-void tourner(int direction, float angle);
-void tournerCentre(int direction, float angle);
-void danse(/*float angle*/);
-float distance_mm_pulse(float distance_mm);
-void avancer(float distance_mm);
-void reculer(float distance_mm);
-void tourner_reculer(int direction, float angle);
+    //DÉCOMMENTEZ LE #define du robot que vous voulez utiliser SEULEMENT
+    //#define ROBOTAUTONOME
+    //#define ROBOTMANUEL
 
+    //Si 2 robots définis, dé-defini les deux codes
+    #ifdef ROBOTAUTONOME
+        #ifdef ROBOTMANUEL
+            #undef ROBOTAUTONOME
+            #undef ROBOTMANUEL
+        #endif
+    #endif
+
+    // ----- A U T O N O M E ------
+    #ifdef ROBOTAUTONOME
+        //Fonctions du robot autonome ici
+    #endif
+
+    // ----- M A N U E L -----
+    #ifdef ROBOTMANUEL
+        //Fonctions du robot manuel ici
+
+    #endif
 #endif
--- LeornadUS/src/main.ino
+++ LeornadUS/src/main.ino
@@ -5,7 +5,7 @@ Auteurs: Simon St-Onge, Philippe B-L, Éric Leduc, Sébastien St-Denis
 Description: Parcours de LéonardUS
 Date: 11 octobre 2018
 ============================================*/
-#include "mesFonctions.h"
+#include <mesFonctions.h>
 /* ****************************************************************************
 Fonctions d'initialisation (setup)
 **************************************************************************** */
@@ -22,7 +22,7 @@ Fonctions de boucle infini (loop())
 // -> Se fait appeler perpetuellement suite au "setup"
 void loop()
  {
-
+   danse();
   // SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
   delay(10);// Delais pour décharger le CPU
 
